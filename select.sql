6ad21e646cbe07e128e2a6e4b57a161048efe94d
Richard Widen <richard.widen@mimer.com>
2021-06-10 17:35:28 +0200

    Added trigger

--- select.sql
+++ select.sql
@@ -6,7 +6,7 @@ select count(*) from bill left join BOOK_GUEST BG on BILL.RESERVATION = BG.RESER
 select count(*) from bill left join BOOK_GUEST BG on BILL.RESERVATION = BG.RESERVATION where cast(ON_DATE as DATE)=CHECKIN;
 
 --Distinct
-select distinct(RESERVED_FNAME )from BOOK_GUEST b;
+select count(distinct(RESERVED_FNAME)) as guest_names, count(RESERVED_FNAME) as guest_with_name from BOOK_GUEST b;
 
 -- Where
 select * from BILL where RESERVATION=1351;
@@ -28,10 +28,15 @@ select
         then RESERVED_FNAME
         else GUEST_FNAME
     end as f_name
+from BOOK_GUEST;select
+    case when GUEST_FNAME IS NULL
+        then RESERVED_FNAME
+        else GUEST_FNAME
+    end as f_name
 from BOOK_GUEST;
 
 -- Group by /HAVING
-select B.RESERVATION, count(b.CHARGE_CODE) from BOOK_GUEST left join BILL B on BOOK_GUEST.RESERVATION = B.RESERVATION group by B.RESERVATION;
+select B.RESERVATION, count(*) from BOOK_GUEST left join BILL B on BOOK_GUEST.RESERVATION = B.RESERVATION group by B.RESERVATION;
 select B.RESERVATION, count(b.CHARGE_CODE) from BOOK_GUEST left join BILL B on BOOK_GUEST.RESERVATION = B.RESERVATION group by B.RESERVATION having count(b.CHARGE_CODE)>10;
 
 
@@ -41,5 +46,47 @@ select count(*) from BOOK_GUEST BG inner join BILL B on BG.RESERVATION = B.RESER
 
 
 --Subselect
-select b.CHARGE_CODE,  from BILL b left join CHARGES c on b.CHARGE_CODE = c.CHARGE_CODE order by c.CHARGE_PRICE;
+select b.CHARGE_CODE  from BILL b left join CHARGES c on b.CHARGE_CODE = c.CHARGE_CODE order by c.CHARGE_PRICE;
 select b.CHARGE_CODE from bill b where b.charge_code=(select c.charge_code from charges c order by c.charge_price fetch first row);
+
+-- View simple
+create view checked_in_guests as
+select * from BOOK_GUEST BG where checkout is null and HOTELCODE='WIND'
+with check option;
+
+INSERT INTO checked_in_guests (
+    RESERVATION, BOOKING_DATE, HOTELCODE, ROOMTYPE, COMPANY, TELEPHONE, RESERVED_FNAME, RESERVED_LNAME, ARRIVE, DEPART, GUEST_FNAME, GUEST_LNAME, ADDRESS, CHECKIN, CHECKOUT, ROOMNO, PAYMENT)
+VALUES (2000, DATE'2001-03-18', 'WIND', 'NSDBLS', 'Mimer', '0187809200', 'Richard', 'Widén', DATE'2001-06-18', DATE'2001-06-19', '', '', '', DEFAULT, DEFAULT, 'LAP200', '');
+select * from checked_in_guests;
+
+-- View With on insert (fails)
+create view checked_in_guests as
+select BG.RESERVATION,BG.RESERVED_FNAME,BG.RESERVED_LNAME from BOOK_GUEST BG where checkout is null and HOTELCODE='WIND'
+with check option;
+INSERT INTO checked_in_guests (RESERVED_FNAME,RESERVED_LNAME) values(default,'Richard','Widén')
+select * from checked_in_guests;
+
+-- Adding trigger
+@delimiter %%%;
+CREATE TRIGGER ins_check_in
+    instead of INSERT
+    on checked_in_guests
+    referencing new table as new_tab
+for each statement
+begin atomic
+insert into BOOK_GUEST(
+    RESERVATION, BOOKING_DATE, HOTELCODE, ROOMTYPE, COMPANY, TELEPHONE,
+    RESERVED_FNAME,RESERVED_LNAME,
+    ARRIVE, DEPART, GUEST_FNAME, GUEST_LNAME, ADDRESS, CHECKIN, CHECKOUT, ROOMNO, PAYMENT)
+    VALUES (
+        (select max(reservation)+1 from BOOK_GUEST) , DATE'2001-03-18', 'WIND', 'NSDBLS', 'Mimer', '0187809200',
+        (select RESERVED_FNAME from new_tab ), (select RESERVED_LNAME from new_tab),
+        DATE'2001-06-18', DATE'2001-06-19', '', '', '', DEFAULT, DEFAULT, 'LAP200', '');
+end%%%
+@delimiter ;
+%%%
+
+INSERT INTO checked_in_guests (RESERVED_FNAME,RESERVED_LNAME) values('Richard','Widén');
+select * from checked_in_guests;
+
+--
